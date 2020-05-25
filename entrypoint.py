2f9c34735230177becbb7247a353ff49142b729e
Andreas Neumeier <andreas@neumeier.org>
2020-05-25 11:10:28 +0200

    more like example.

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,11 @@
 #!/bin/env python3
 
 import sys
+import datetime
+
 
 if __name__=='__main__':
-    print(sys.argv)
-    print("Hello, world!")
+    hello = sys.argv[1]
+    time = datetime.datetime.now()
+    print(f"Hello {hello}")
+    print(f"::set-output name=time::{time}")
